0b745eaafb92d7760cad41ed2f9d0adf0a554e84
Mirudhubhashini-p <97428759+Mirudhubhashini-p@users.noreply.github.com>
2022-04-25 19:18:09 +0530

    Create Course.js

---
+++ Course.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<h<!DOCTYPE html>
+<html>
+<body>
+tml>
+<body<!DOCTYPE html>
+<h<!DOCTYPE html>
+<html>
+<body<!DOCTYPE html>
+<h<!DOCTYPE html>
+<html>
+<body>
+tml>
+<body>
+>
+tml>
+<body>
+>
+
+<h2>My First Web Page</h2>
+
+<p>Activate</p>
+
+<script>
+a = 5;
